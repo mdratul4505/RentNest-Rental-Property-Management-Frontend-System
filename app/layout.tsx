bb8a772e3fe171ddf97c6d080c9b6c2c323384b9
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-28 23:14:27 +0600

    added navar form shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
+import "./globals.css";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn("h-full antialiased", "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+
+        <Toaster position="top-right" richColors />
+        {/* Navbar */}
+        {children}
+
+        {/* Footer */}
+      </body>
     </html>
   );
 }
---
+++ app/loding.tsx
@@ -0,0 +1,4 @@
+
+export default function Loading() {
+  return <div className="flex h-screen items-center justify-center">Loading...</div>;
+}
