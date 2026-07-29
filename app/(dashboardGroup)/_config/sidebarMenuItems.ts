fd6648367ad924f42faeea70bc623556c0e369cc
mdratul4505 <dev.raul.coder@gmail.com>
2026-07-30 02:23:39 +0600

    adddded landloard sidebar

--- app/(dashboardGroup)/_config/sidebarMenuItems.ts
+++ app/(dashboardGroup)/_config/sidebarMenuItems.ts
@@ -1,21 +1,10 @@
 import { ISidebarItem } from "@/lib/types"
 import { FileText, LayoutDashboard } from "lucide-react"
 import { ADMIN_SIDEBAR_ITEMS } from "./adminSidebarItems"
-import { AUTHOR_SIDEBAR_ITEMS } from "./authorSidebarItems"
+// import { AUTHOR_SIDEBAR_ITEMS } from "./authorSidebarItems"
 
 
-const USER_SIDEBAR_ITEMS : ISidebarItem[] = [
-    {
-        label : "Dashboard",
-        href : "/dashboard",
-        icon : LayoutDashboard
-    },
-    {
-        label : "My Posts",
-        href : "/dashboard/my-posts",
-        icon : FileText
-    },
-]
+
 
 
 export const sidebarMenuItems = {
